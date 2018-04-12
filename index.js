a3ef455b4cb72daeddac48ca8ee5dab0e41dcad8
pramanis <SUMIT.PRAMANIK@anz.com>
2018-04-13 00:03:07 +1000

    deleteMany operation in mongoDb

--- index.js
+++ index.js
@@ -5,19 +5,25 @@ var url = 'mongodb://127.0.0.1:27017/';
 MongoClient.connect(url, function(err, db) {
     if(err) throw err;
     var dbo = db.db("mydb");
-    dbo.createCollection("customers", function(err,res) {
+    /*dbo.createCollection("customers", function(err,res) {
         if(err) throw err;
         console.log("Collection created");
         var myObj = {name : "Company 1", address:"Highway 37"};
         dbo.collection('customers').insertOne(myObj, function(err,res) {
             if(err) throw err;
             console.log("record inserted");
-            var mySort = {name:-1};
-            dbo.collection('customers').find().sort(mySort).toArray(function(err, result) {
+            var query = {name:"Company 1"}
+            dbo.collection('customers').deleteOne(query,function(err, result) {
                 if(err) throw err;
-                console.log(result);
+                console.log(result.deletedCount);
                 db.close();
             });
         });
-    });
+    });*/
+    var query = {name:"Company 2"};
+    dbo.collection('customers').deleteMany(query, function(err,result) {
+        if(err) throw err;
+        console.log(result.deletedCount);
+        db.close();
+    })
 });
