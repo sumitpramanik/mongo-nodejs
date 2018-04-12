7e044c4b01f070bf26b54c4319608e0116f0659f
pramanis <SUMIT.PRAMANIK@anz.com>
2018-04-13 00:30:26 +1000

    Limit function

--- index.js
+++ index.js
@@ -5,10 +5,24 @@ var url = 'mongodb://127.0.0.1:27017/';
 MongoClient.connect(url, function(err, db) {
     if(err) throw err;
     var dbo = db.db('mydb');
-    //drop database
-    dbo.collection('customers').drop(function(err,delOK) {
+    var myArray = [
+        { name: 'John', address: 'Highway 71'},
+        { name: 'Peter', address: 'Lowstreet 4'},
+        { name: 'Amy', address: 'Apple st 652'},
+        { name: 'Hannah', address: 'Mountain 21'},
+        { name: 'Michael', address: 'Valley 345'},
+        { name: 'Sandy', address: 'Ocean blvd 2'},
+        { name: 'Betty', address: 'Green Grass 1'},
+        { name: 'Richard', address: 'Sky st 331'},
+        { name: 'Susan', address: 'One way 98'},
+        { name: 'Vicky', address: 'Yellow Garden 2'},
+        { name: 'Ben', address: 'Park Lane 38'},
+        { name: 'William', address: 'Central st 954'},
+        { name: 'Chuck', address: 'Main Road 989'},
+        { name: 'Viola', address: 'Sideway 1633'}];
+    dbo.collection('customers').find().limit(5).toArray(function(err,res) {
         if(err) throw err;
-        if(delOK) console.log("Collection dropped");
+        console.log('Collection crerated with ', res, 'records');
         db.close();
     });
 });
