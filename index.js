a6f207ce24e9ca6845c43b816c88b1c6d41cbf83
pramanis <SUMIT.PRAMANIK@anz.com>
2018-04-12 21:28:00 +1000

    Creating Db,Collection,insert record and find one

---
+++ index.js
@@ -0,0 +1,21 @@
+var mongo = require('mongodb');
+var MongoClient = mongo.MongoClient;
+var url = 'mongodb://127.0.0.1:27017/';
+
+MongoClient.connect(url, function(err, db) {
+    if(err) throw err;
+    var dbo = db.db("mydb");
+    dbo.createCollection("customers", function(err,res) {
+        if(err) throw err;
+        console.log("Collection created");
+        var myObj = {name : "Company 1", address:"Highway 37"};
+        dbo.collection('customers').insertOne(myObj, function(err,res) {
+            if(err) throw err;
+            dbo.collection('customers').findOne({}, function(err, result) {
+                if(err) throw err;
+                console.log(result.name);
+                db.close();
+            });
+        });
+    });
+});
